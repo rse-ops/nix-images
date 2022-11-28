778a2eb135a2cb71cbadabea7310fcffa60b0a4d
vsoch <vsoch@users.noreply.github.com>
2022-11-28 13:40:53 -0700

    cd into basedir
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- .github/scripts/build_simple.sh
+++ .github/scripts/build_simple.sh
@@ -9,7 +9,9 @@ printf "Base directory is ${basedir}\n"
 echo "${prefix} -t ${container} ."
 echo "Current directory is ${PWD}"
 ls
+cd ${basedir}
 ${prefix} -f $filename -t ${container} .
+cd -
 
 echo "uri=${container}" >> $GITHUB_OUTPUT
 echo "dockerfile_dir=${basedir}" >> $GITHUB_OUTPUT
